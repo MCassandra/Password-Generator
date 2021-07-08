f5a64d697b6f5bc2540ed9f64feac574c45b613d
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-07-07 21:49:56 -0700

    changing variable placements

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var options = [];
+
+var upperCase="ABCDEFGHIJKLMNOPRQSTUVWXYZ".split("")
+var lowerCase="abcdefghijklmnopqrstuvwxyz".split("")
+var numbers="1234567890".split("")
+var symbols="!@#%^&*()?/".split("")
+var characters = prompt ("How many characters do you want?");
+var wantsuppercase= confirm ("Do you want upper case letters?")
+var wantslowercase=confirm ("Do you want lower case letters?")
+var wantsnumbers=confirm ("Do you want numbers?")
+var wantssymbols=confirm ("Do you want symbols?")
 
 function generatePassword(){
-  var options = [];
 
-  var upperCase="ABCDEFGHIJKLMNOPRQSTUVWXYZ".split("")
-  var lowerCase="abcdefghijklmnopqrstuvwxyz".split("")
-  var numbers="1234567890".split("")
-  var symbols="!@#%^&*()?/".split("")
-  var characters = prompt ("How many characters do you want?");
-  var wantsuppercase= confirm ("Do you want upper case letters?")
-  var wantslowercase=confirm ("Do you want lower case letters?")
-  var wantsnumbers=confirm ("Do you want numbers?")
-  var wantssymbols=confirm ("Do you want symbols?")
 
   var index = Math.floor(Math.random() * options.length);
   
@@ -21,22 +22,22 @@ if (characters<8||characters>128){
     // if characters less than 128, move to next prompt
     alert ("password must contain atleast 8 characters.");
   }
-if (wantsuppercase){
-  index=options.concat(upperCase);
-  return;
-}
-if (wantslowercase){
-  options=options.concat(uppercase,lowerCase);
-  return;
-}
-if (wantsnumbers){
-  options=options.concat(uppercase,lowercase,numbers);
-  return;
-}
-if (wantssymbols){
-  options=options.concat(uppercase,lowercase,numbers,symbols);
-  return;
-}
+// // if (wantsuppercase){
+//   options=options.concat(upperCase);
+//   return;
+// }
+// // if (wantslowercase){
+//   options=options.concat(lowerCase);
+//   return;
+// }
+// if (wantsnumbers){
+//   options=options.concat(numbers);
+//   return;
+// }
+// if (wantssymbols){
+//   options=options.concat(symbols);
+//   return;
+// }
   
 }
 
